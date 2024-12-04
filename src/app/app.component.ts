0b8bf7cc3d16191c6ffc3c08d781a4573eebe2c2
seyjik <seyjik@gmail.com>
2024-12-04 13:43:05 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CvComponent } from './cv/cv.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+    
+
+    {path : '', component:HomeComponent},
+    {path : 'cv', component:CvComponent},
+    {path:'home',component:HomeComponent},
+    {path:'projects',component:ProjectsComponent},
+    {path:'contact',component:ContactComponent},
+    { path: '**', redirectTo: '' } 
+];
